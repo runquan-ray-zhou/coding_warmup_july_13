e4a251b55b1cf035557a1e4510949037321db7f6
RazEfron <razefron01@gmail.com>
2024-07-13 08:32:20 -0400

    initial commit

---
+++ posts.sql
@@ -0,0 +1,50 @@
+-- To run this file, use the following command:
+-- psql -f posts.sql
+
+DROP TABLE IF EXISTS posts;
+DROP DATABASE IF EXISTS blog;
+CREATE DATABASE blog;
+
+\c blog
+
+
+-- Create posts table
+-- id SERIAL PRIMARY KEY,
+-- title VARCHAR(255),
+-- body TEXT,
+-- created_at TIMESTAMP,
+-- updated_at TIMESTAMP
+
+
+
+-- Alter posts table to include user name
+
+
+
+-- Insert 5 posts into posts table
+
+
+
+-- Delete a post with id of 1
+
+
+
+-- Update the post with id of 2 to have the title "New Title"
+
+
+
+-- Create a users table
+-- id SERIAL PRIMARY KEY,
+-- name VARCHAR(255),
+-- email VARCHAR(255),
+-- password_digest VARCHAR(255),
+-- created_at TIMESTAMP,
+-- updated_at TIMESTAMP
+
+
+
+-- Insert 5 users into users table
+
+
+
+-- Update the posts table to include a foreign key for the user who created the post
